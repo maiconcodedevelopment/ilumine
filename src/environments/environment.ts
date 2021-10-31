015013f5fb8130cd687baf45d4a2ca3b594ce0e8
Maicon Douglas <35504270+maiconcodedevelopment@users.noreply.github.com>
2021-10-31 08:29:17 -0300

    prod environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   //apiUrl: 'https://ilumine-srv.azurewebsites.net/api/v1'
-  apiUrl: 'https://localhost:44388/api/v1'
+  apiUrl: 'https://ilumine-service.azurewebsites.net/api/v1'
 };
 
 /*
